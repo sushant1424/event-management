3bf902e7b48ecfad05679dbec862310e0c6b8845
sushant1424 <sushantshrestha184@gmail.com>
2024-04-06 21:44:57 +0545

    create add,update and delete categories code

--- admin/alter_code.php
+++ admin/alter_code.php
@@ -15,7 +15,7 @@ if(isset($_POST['delete_user']))
   }
   else
   {
-    $_SESSION['message'] = "Couldnot add user/admin";
+    $_SESSION['message'] = "Couldnot delete user/admin";
     header("Location: view_users.php");
     exit(0);
   }
@@ -101,4 +101,60 @@ if(isset($_POST['add_category'])){
   }
 
 }
+
+//-------update category-------//
+
+if(isset($_POST['update_category']))
+{
+  $category_id = $_POST['category_id'];
+  $name = $_POST['cname'];
+  $slug = $_POST['slug'];
+  $description = $_POST['description'];
+  $meta_title = $_POST['meta_title'];
+  $meta_description = $_POST['meta_description'];
+  $meta_keyword = $_POST['meta_keyword'];
+  $navbar_status = $_POST['navbar_status'] == true ? '1':'0';
+  $status = $_POST['status'] == true ? '1' : '0';
+
+  $query = "UPDATE categories SET name = '$name', slug = '$slug', description = '$description' , meta_title = '$meta_title' , meta_description = '$meta_description', meta_keyword = '$meta_keyword' ,navbar_status = '$navbar_status' ,
+  status = '$status' where c_id = '$category_id'";
+
+  $query_run = mysqli_query($conn, $query);
+  if($query_run)
+  {
+    $_SESSION['message'] = "Updated Successfully";
+    header("Location: view_category.php");
+    exit(0);
+  }
+  else
+  {
+    $_SESSION['message'] = "Update Failed";
+    header("Location: view_category.php");
+    exit(0);
+  }
+  }
+
+  //---------delete category------------//
+
+  if(isset($_POST['delete_category']))
+{
+  $category_id = $_POST['delete_category'];
+  //2==delete
+  $query = "UPDATE categories SET status ='2' where c_id = '$category_id' LIMIT 1";
+  $query_run = mysqli_query($conn, $query);
+  if($query_run)
+  {
+    $_SESSION['message'] = "Deleted Successfully";
+    header("Location: view_category.php");
+    exit(0);
+  }
+  else
+  {
+    $_SESSION['message'] = "Couldnot delete";
+    header("Location: view_category.php");
+    exit(0);
+  }
+
+}
+
 ?>
